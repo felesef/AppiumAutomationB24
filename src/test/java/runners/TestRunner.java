2b88fba8f989ed0bee1a21316ad60adf06c63fb0
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2022-02-26 12:51:14 -0500

    update

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,15 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "step_definitions",
+        features = "src/test/resources",
+        dryRun = false,
+        tags = ""
+)
+public class TestRunner {
+}
